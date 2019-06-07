b51dfaf9292ad4ef12dd592c5be4484ae9e4165a
Túlio <francatm@outlook.com>
2019-06-07 14:54:58 -0300

    create row view

---
+++ landmarks-swiftui/LandmarkRow.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkRow.swift
+//  landmarks-swiftui
+//
+//  Created by Tulio Marcos Franca on 07/06/19.
+//  Copyright © 2019 learn. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow : View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image(forSize: 50)
+            Text(landmark.name)
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarkRow_Previews : PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
+#endif
